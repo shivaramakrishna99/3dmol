124bb4ab3526e65e2aed5358d9123f60968e6020
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2022-11-17 21:44:05 +0530

    new metadata objs

--- wf/__init__.py
+++ wf/__init__.py
@@ -14,12 +14,14 @@ import py3Dmol
 @large_task
 def _3dmol_task(
     mol: Union[LatchFile, str],
+    selectionName1: str = "name1",
 ) -> LatchFile:
 
     _3dmol_frame = f'''
      <script src="https://3Dmol.org/build/3Dmol-min.js" async></script>     
-    <div style="height: 100%; width: 400px; position: relative;" class='viewer_3Dmoljs' data-pdb='1YCR' 
+    <div style="height: 100%; width: 100%; position: relative;" class='viewer_3Dmoljs' data-pdb='1YCR' 
     data-select1='chain:A' data-style1='cartoon:color=spectrum' data-surface1='opacity:.7;color:white' data-select2='chain:B' data-style2='stick'></div>       
+    
     '''
     view = 'view.html'
     with open(view, 'w') as f:
@@ -41,14 +43,14 @@ metadata = LatchMetadata(
     license="GPL-3.0",
     parameters= {
         "mol": LatchParameter(
-            display_name="Molecule",
+            display_name="Molecule ",
             description="Enter PDB ID, upload a file, or provide a URL",
         ),
-        "a": LatchParameter(
-            display_name="List",
-            description="List desc"
+        "selectionName1": LatchParameter(
+            display_name="First Selection",
+            section_title="First Selection",
+            description="Name your first selection"
         )
-
     },
     tags=[
         'Visualisation'
@@ -59,12 +61,14 @@ metadata = LatchMetadata(
 @workflow(metadata)
 def _3dmol_wf(
     mol: Union[LatchFile, str],
+    selectionName1: str = "name1",
 ) -> LatchFile:
     """3dmol DESCRIPTION
     LONG DESC
     """
     return _3dmol_task(
-        mol=mol
+        mol=mol,
+        selectionName1=selectionName1
         )
 
 
